a92623d1440686c644d0d61c22305fae0591b014
xndcmd <xndcmd@gmail.com>
2022-07-27 11:23:34 -0700

    adjusted comments

--- bucket-list-app/src/components/Bucket.js
+++ bucket-list-app/src/components/Bucket.js
@@ -12,9 +12,9 @@ function Bucket(props) {
 
   const submitUpdate = (value) => {
 
-    // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
+    // Write logic to update the `edit` value in state after a user updates an entry in the list
 
-    // TODO: Set the key:value pairs in the `edit` object back to empty strings
+    // Set the key:value pairs in the `edit` object back to empty strings
     props.editBucketItem(edit.id, value);
     setEdit({ id: null, value: '', eagerness: '' });
   };
@@ -25,22 +25,22 @@ function Bucket(props) {
   }
 
   return props.bucket.map((item, index) => (
-    // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
-    // TODO: Add a key attribute set to the value of the index position
-    // Hint: use a ternary operator
+    // Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
+    // Add a key attribute set to the value of the index position
+    // use a ternary operator
     <div className={ item.isComplete
       ? `bucket-row complete ${item.eagerness}`
       : `bucket-row ${item.eagerness}` } key={i}>
 
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
+      {/* Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument */}
       <div key={item.id} onClick={() => props.completeBucketItem(item.id)}>
-          {/* TODO: Add the item text here */}
+          {/* Add the item text here */}
           {item.id}
       </div>
       <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
+        {/* // Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties */}
         <p onClick={() => setEdit({ id: item.id, value: item.text, eagerness: item.eagerness })}> ✏️</p>
-        {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
+        {/* Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
         <p onClick={() => props.removeBucketItem(item.id)}> 🗑️</p>
       </div>
     </div>
