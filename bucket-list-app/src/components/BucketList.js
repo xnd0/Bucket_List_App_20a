23581de5b696f3c737acb330d9ad6c8d168228e6
xndcmd <xndcmd@gmail.com>
2022-07-27 11:35:23 -0700

    added functionality to mark an item as complete or incomplete

--- bucket-list-app/src/components/BucketList.js
+++ bucket-list-app/src/components/BucketList.js
@@ -33,7 +33,11 @@ function BucketList() {
     // If the ID passed to this function matches the ID of the item that was clicked, mark it as complete
     let updatedBucket = bucket.map((item) => {
       
-      // TODO: Write logic that marks an item as complete or incomplete when invoked
+      // Logic that marks an item as complete or incomplete when invoked
+      if (item.id === id) {
+        item.isComplete = !item.isComplete;
+      }
+      return item;
 
     });
 
